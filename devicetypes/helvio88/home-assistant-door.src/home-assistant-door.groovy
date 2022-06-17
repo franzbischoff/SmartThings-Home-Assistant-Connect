48c06cef3650906f48b80af77b9aff0d51b3aa4b
Helvio Pedreschi <helvio88@gmail.com>
2022-06-17 15:27:53 -0400

    Garage Doors

--- devicetypes/helvio88/home-assistant-door.src/home-assistant-door.groovy
+++ devicetypes/helvio88/home-assistant-door.src/home-assistant-door.groovy
@@ -15,33 +15,9 @@
  */
 metadata {
 	definition (name: "Home Assistant Door", namespace: "Helvio88", author: "Helvio Pedreschi") {
-		capability "Actuator"
 		capability "Polling"
 		capability "Refresh"
-		capability "Sensor"
 		capability "Door Control"
-	}
-
-
-	simulator { }
-
-	tiles(scale: 2) {
-		standardTile("toggle", "device.door", width: 2, height: 2) {
-			state("closed", label:'${name}', action:"door control.open", icon:"st.doors.garage.garage-closed", backgroundColor:"#00A0DC", nextState:"opening")
-			state("open", label:'${name}', action:"door control.close", icon:"st.doors.garage.garage-open", backgroundColor:"#e86d13", nextState:"closing")
-			state("opening", label:'${name}', icon:"st.doors.garage.garage-closed", backgroundColor:"#e86d13")
-			state("closing", label:'${name}', icon:"st.doors.garage.garage-open", backgroundColor:"#00A0DC")
-			
-		}
-		standardTile("open", "device.door", inactiveLabel: false, decoration: "flat") {
-			state "default", label:'open', action:"door control.open", icon:"st.doors.garage.garage-opening"
-		}
-		standardTile("close", "device.door", inactiveLabel: false, decoration: "flat") {
-			state "default", label:'close', action:"door control.close", icon:"st.doors.garage.garage-closing"
-		}
-
-		main "toggle"
-		details(["toggle", "open", "close"])
 	}
 }
 
@@ -56,34 +32,12 @@ def refresh() {
 
 def open() {
 	if (parent.postService("/api/services/cover/open_cover", ["entity_id": device.deviceNetworkId])) {
-    	sendEvent(name: "switch", value: "on")
+    	sendEvent(name: "door", value: "open")
     }
 }
 
 def close() {
 	if (parent.postService("/api/services/cover/close_cover", ["entity_id": device.deviceNetworkId])) {
-    	sendEvent(name: "switch", value: "off")
+    	sendEvent(name: "door", value: "closed")
     }
-}
-
-def presetPosition(percent) {
-	def state = (percent == 0 ? "off" : "on")
-    
-    if (parent.postService("/api/services/cover/set_cover_position", ["entity_id": device.deviceNetworkId, "position": percent])) {
-    	sendEvent(name: "level", value: percent)
-    	sendEvent(name: "switch.setLevel", value: percent)
-        sendEvent(name: "switch", value: state)
-    }
-}
-
-def on() {
-	open()
-}
-
-def off() {
-	close()
-}
-
-def setLevel(percent) {
-	presetPosition(percent)
 }
